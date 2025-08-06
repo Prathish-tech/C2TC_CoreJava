0fb0e21ec6f32193bd2a4f7f1d761f06a7450532
your-github-username <prathishs.2004@gmail.com>
2025-08-06 10:43:23 +0530

    Initial commit

---
+++ src/DayFifteen/Generics/GenericClass.java
@@ -0,0 +1,15 @@
+package DayFifteen.Generics;
+
+class GenericClass<T>
+{
+	T data;
+	void set(T data)
+	{
+		this.data=data;
+	}
+	
+	T get()
+	{
+		return data;
+	}
+}
---
+++ src/DayFifteen/Generics/MinMax.java
@@ -0,0 +1,8 @@
+package DayFifteen.Generics;
+
+public interface MinMax<T extends Comparable<T>> {
+
+	T min();
+
+	T max();
+}
